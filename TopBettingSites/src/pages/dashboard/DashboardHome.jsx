ae87057e2cf495de9df7e7f231bd9a89c7a01889
IbrahemMohammad09 <137792138+IbrahemMohammad09@users.noreply.github.com>
2025-08-16 05:40:39 +0300

    Update DashboardHome.jsx

--- TopBettingSites/src/pages/dashboard/DashboardHome.jsx
+++ TopBettingSites/src/pages/dashboard/DashboardHome.jsx
@@ -20,13 +20,13 @@ const DashboardHome = () => {
             <h1 className='items-center justify-center text-center text-3xl'>Welcome to Dashboard</h1>
             <div className='flex flex-col'>
                 {Categories.map((item,index) => (
-                    <div className="flex flex-row mt-8 bg-gray-700 m-4 p-4 items-center justify-between">
-                    <h1 className="text-white font-bold">{item.name}</h1>
-                    <div className="flex space-x-4">
-                      <MdDeleteForever className="cursor-pointer text-red-500 hover:text-red-700" />
-                      <MdModeEditOutline className="cursor-pointer text-yellow-400 hover:text-yellow-600" />
+                    <div key={index} className="flex flex-row mt-8 bg-gray-700 m-4 p-4 items-center justify-between">
+                        <h1 className="text-white font-bold">{item.name}</h1>
+                        <div className="flex space-x-4">
+                        <MdDeleteForever className="cursor-pointer text-red-500 hover:text-red-700" />
+                        <MdModeEditOutline className="cursor-pointer text-yellow-400 hover:text-yellow-600" />
+                        </div>
                     </div>
-                  </div>
                   
                 ))}
             </div>
